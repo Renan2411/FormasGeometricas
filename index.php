8df72f9dc227bbea8b18e3c4900a5b1ffbaf47ce
Renan2411 <51676653+Renan2411@users.noreply.github.com>
2019-09-16 22:47:18 -0400

    bla

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="page.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body class="pg-light">
 	<section id="home" class="content">
 		<article class="box">
 			<h2>Home</h2>
-			VAMO COLOCAR O PLUGIN DE SLIDE
+
 		</article>
 	</section>
 
@@ -129,7 +130,12 @@
 			<h2>Tetraedro</h2>	 
 			<form class="needs-validation" action="calculo.php" method="post">
 				<div class="row">
-					<!-- Colocar o campo para pedir -->
+					
+					<div class="col-md-2 mb-3">
+						<label for="l">Lado</label>
+						<input type="number" min="0" name="l" id="l" class="form-control" placeholder="Digite em cm">
+					</div>
+
 					<div class="col-md-2 mb-3">
 						<label for="cal">Escolha</label>
 						<select class="custom-select d-block w-100" required name="cal" id="cal">
@@ -329,16 +335,16 @@
 		<nav>
 			<ul>
 				<li id="link-home"><a href="#home">&nbsp;Home</a></li>
-				<li id="link-company"><a href="#company1">&nbsp;Esfera</a></li>
-				<li id="link-company"><a href="#company2">&nbsp;Pirâmide de Base Quadrangular</a></li>
-				<li id="link-company"><a href="#company3">&nbsp;Cilindro</a></li>
-				<li id="link-company"><a href="#company4">&nbsp;Tetraedro</a></li>
-				<li id="link-company"><a href="#company5">&nbsp;Hexaedro</a></li>
-				<li id="link-company"><a href="#company6">&nbsp;Octaedro</a></li>
-				<li id="link-company"><a href="#company7">&nbsp;Dodecaedro</a></li>
-				<li id="link-company"><a href="#company8">&nbsp;Icosaedro</a></li>
-				<li id="link-company"><a href="#company9">&nbsp;Poliedro L</a></li>
-				<li id="link-company"><a href="#company10">&nbsp;Poliedro Mais</a></li>
+				<li id="link-company1"><a href="#company1">&nbsp;Esfera</a></li>
+				<li id="link-company2"><a href="#company2">&nbsp;Pirâmide de Base Quadrangular</a></li>
+				<li id="link-company3"><a href="#company3">&nbsp;Cilindro</a></li>
+				<li id="link-company4"><a href="#company4">&nbsp;Tetraedro</a></li>
+				<li id="link-company5"><a href="#company5">&nbsp;Hexaedro</a></li>
+				<li id="link-company6"><a href="#company6">&nbsp;Octaedro</a></li>
+				<li id="link-company7"><a href="#company7">&nbsp;Dodecaedro</a></li>
+				<li id="link-company8"><a href="#company8">&nbsp;Icosaedro</a></li>
+				<li id="link-company9"><a href="#company9">&nbsp;Poliedro L</a></li>
+				<li id="link-company10"><a href="#company10">&nbsp;Poliedro Mais</a></li>
 				<li id="link-service"><a href="#service">&nbsp;Serviços</a></li>
 				<li id="link-contact"><a href="#contact">&nbsp;Contato</a></li>
 			</ul>
